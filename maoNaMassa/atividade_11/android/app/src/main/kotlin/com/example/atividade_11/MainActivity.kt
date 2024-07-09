5ebc809c0481310f0bcfda6706ee37ee26cc3850
Marco <mbarozzi@gmail.com>
2024-07-09 04:23:51 -0300

    removendo pasta de testes

--- maoNaMassa/atividade_11/android/app/src/main/kotlin/com/example/atividade_11/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.atividade_11
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- maoNaMassa/atividade_11/lib/counter_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
-
-class CounterCubit extends Cubit<int> {
-   CounterCubit():super(0);
-   
-   void increment()=>emit(state+1);
-
-   }
